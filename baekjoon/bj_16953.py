03221b5bdf7c1a0b04559d7248c843e140bc2011
SONEUNBI <treach639@gmail.com>
2023-07-23 17:55:10 +0900

    commit

--- baekjoon/bj_16953.py
+++ baekjoon/bj_16953.py
@@ -2,5 +2,16 @@ import sys
 input = sys.stdin.readline
 
 a,b = map(int,input().split())
+cnt=1
 while a!=b:
-    multi = a*2
+    tmp =b
+    if b%10 ==1:
+        b//=10
+        cnt+=1
+    elif b%2==0:
+        b//=2
+        cnt+=1
+    if tmp == b:
+        cnt = -1
+        break
+print(cnt)
