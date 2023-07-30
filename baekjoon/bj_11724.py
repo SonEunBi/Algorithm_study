fb0e5b9e86b66ff58d668822f58dfcd0257df63b
SONEUNBI <treach639@gmail.com>
2023-07-31 01:37:41 +0900

    commit

--- baekjoon/bj_11724.py
+++ baekjoon/bj_11724.py
@@ -2,11 +2,28 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
-visited =[[0] for i in range(n)]
-def dfs(start, end):
-    start, end = map(int, input().split())
-    for i in range(m):
-        visited[start] =1
-        visited[end]=1
-    
-    
+graph=[[] for _ in range(n+1)]
+visited = [False]*(n+1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+cnt =0
+def dfs(i):
+    stack=[]
+    stack.append(i)
+    visited[i] =True
+    while stack:
+        now = stack.pop()
+        for nxt in graph[now]:
+            if not visited[nxt]:
+                stack.append(nxt)
+                dfs(nxt)
+
+for i in range(1, n+1):
+    if not visited[i]:
+        cnt+=1
+        dfs(i)
+print(cnt)
